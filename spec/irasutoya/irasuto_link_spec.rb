11497bb02bacf82b7e7ea0c2a411b966555142fe
ac <ac@domain.local>
2020-10-16 10:45:30 +0900

    add a case for multiple image page

--- spec/irasutoya/irasuto_link_spec.rb
+++ spec/irasutoya/irasuto_link_spec.rb
@@ -25,7 +25,23 @@ RSpec.describe Irasutoya::IrasutoLink do
     end
 
     it 'should return irasuto with image_url' do
-      expect(irasuto_link.fetch_irasuto.image_url).to eq('http://4.bp.blogspot.com/-7Pa9IazPoII/VGLMYk-SjpI/AAAAAAAAo_s/IryhUKoFJQ0/s400/himan03_youngman.png')
+      expect(irasuto_link.fetch_irasuto.image_url).to eq('https://4.bp.blogspot.com/-7Pa9IazPoII/VGLMYk-SjpI/AAAAAAAAo_s/IryhUKoFJQ0/s400/himan03_youngman.png')
+    end
+
+    context 'when there are multiple images on a page' do
+      before do
+        allow(URI).to receive_message_chain(:parse, :open) {
+          File.read('spec/files/show_multiple_images.html')
+        }
+      end
+
+      it 'should return irasuto (postthumb) with image_url' do
+        expect(irasuto_link.fetch_irasuto.image_url).to eq('https://1.bp.blogspot.com/-rchqpx0JGcU/XpKozq0JXAI/AAAAAAABYXA/BnJXB5WNKAcjeFC6_t2XPXwu7wUNAhL3gCNcBGAsYHQ/s195/thumbnail_jitakutaiki.jpg')
+      end
+
+      it 'should return multiple irasutos' do
+        expect(irasuto_link.fetch_irasuto.image_urls).to match Array
+      end
     end
   end
 end
